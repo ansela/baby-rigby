1f9d10e994abd2cbd888b619c60cdf5ea1ca166d
Ashley Norman <ashley.ansell@gmail.com>
2020-10-12 11:22:23 -0400

    fix pdf?

--- src/Games.tsx
+++ src/Games.tsx
@@ -15,7 +15,6 @@ import React, { useState } from "react"
 import { Document, Outline, Page } from "react-pdf"
 
 //@ts-ignore
-import gamesPDF from "./games/shower-games.pdf"
 import { makeStyles } from "@material-ui/styles"
 
 const useStyle = makeStyles({
@@ -84,13 +83,23 @@ const Games = () => {
           <Typography align="center">
             No baby shower would be complete without a few games! Feel free to
             follow along below, or{" "}
-            <Link href={gamesPDF} target="_blank">
+            <Link
+              href={`${process.env.PUBLIC_URL}/shower-games.pdf`}
+              target="_blank"
+            >
               download
             </Link>{" "}
             and print instead. We will go through them together throughout the
             shower!
           </Typography>
-          <Document file={gamesPDF} onLoadSuccess={onDocumentLoadSuccess}>
+          <Document
+            file={`${process.env.PUBLIC_URL}/shower-games.pdf`}
+            onLoadSuccess={onDocumentLoadSuccess}
+            onLoadError={(error) => {
+              console.error("Load Error")
+              console.error(error)
+            }}
+          >
             <Outline onItemClick={onItemClicked} />
             <Page pageNumber={pageNumber || 1} width={getPageWidth()} />
           </Document>
